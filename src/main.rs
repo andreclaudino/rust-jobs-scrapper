4bf63bb5ee8b8c10762c1cdbdc962d56bd835314
Andre Claudino <andre.claudino@talentify.io>
2021-06-11 16:48:27 -0300

    * Save results to cloud storage instead of local file * Improve error management to reduce panic * Add category and job title to output results

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod openings_navigate;
 mod utils;
 mod job_entity;
 mod job_page;
+mod gs_client;
+
 use serde_json;
 use std::sync::mpsc::channel;
 
@@ -16,6 +18,7 @@ use utils::FantocciniResult;
 use uuid::Uuid;
 use std::io::Write;
 use std::fs::File;
+
 
 
 #[tokio::main]
@@ -36,8 +39,7 @@ async fn main() -> Result<(), fantoccini::error::CmdError> {
     }
 }
 
-
-async fn initial_processing(c: Client, args: Cli) -> FantocciniResult<Client>{
+async fn initial_processing(c: Client, args: &Cli) -> FantocciniResult<Client>{
     let c = force_to_ideed_com(c, args.indeed_url.as_str()).await?;
     let c = navigate_to_browse_jobs_page(c).await?;
     let c = navigate_to_category_page(c, args.category_title.as_str()).await?;
@@ -48,8 +50,7 @@ async fn initial_processing(c: Client, args: Cli) -> FantocciniResult<Client>{
 }
 
 async fn process_page(c: Client, args: Cli) -> FantocciniResult<()> {
-    let output_folder = args.output_folder.clone();
-    let c = initial_processing(c.clone(), args).await?;
+    let c = initial_processing(c.clone(), &args).await?;
 
     let (jobs_sender, jobs_receiver) = channel();
 
@@ -57,17 +58,46 @@ async fn process_page(c: Client, args: Cli) -> FantocciniResult<()> {
         process_openings_list_page(c, jobs_sender).await.unwrap();
     });
 
-    while let Ok(jobs) = jobs_receiver.recv() {
+    let gcloud_client = cloud_storage::Client::default();
 
-        let file_path = format!("{}/{}.json", output_folder, Uuid::new_v4());
-        let file_create_error_message = format!("Erro creating file {}", file_path);
-        let mut output_file = File::create(file_path).expect(file_create_error_message.as_str());
+    let output_folder = args.output_folder.clone();
+    let no_protocol_output_folder =
+        output_folder
+            .clone()
+            .replacen("gs://", "", 1);
 
-        for job in jobs.iter() {
+    let parsed_output_folder: Vec<_> =
+        no_protocol_output_folder
+        .splitn(2, "/")
+        .collect();
+
+    let bucket = parsed_output_folder.get(0).unwrap().to_string();
+    let key_preffix = parsed_output_folder.get(1).unwrap();
+
+    while let Ok(mut jobs) = jobs_receiver.recv() {
+
+        let file_name = format!("{}.json", Uuid::new_v4());
+        let local_file_path = std::env::temp_dir().join(&file_name);
+        let key = format!("{}/{}", key_preffix, file_name);
+
+        let file_create_error_message = format!("Erro creating file {:?}", local_file_path);
+        let mut temporary_file = File::create(&local_file_path).expect(file_create_error_message.as_str());
+
+        for job in jobs.iter_mut() {
+            job.set_category(&args.category_title, &args.job_title);
             let buffer = format!("{}\n", serde_json::to_string(job).unwrap());
-            output_file.write(buffer.as_bytes())?;
+            temporary_file.write(buffer.as_bytes())?;
         }
-    }
+
+        match gs_client::upload_to_gs(&gcloud_client, &local_file_path, &bucket, &key).await {
+            Ok(_) => continue,
+            Err(err) => {
+                eprintln!("Error while uploading file gs://{}/{}: {}", &bucket, &key, err)
+            }
+        };
+        std::fs::remove_file(local_file_path)?;
+    };
+
 
     Ok(())
 }
